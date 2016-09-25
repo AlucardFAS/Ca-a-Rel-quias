961717829f1a2bac7ceeafe834171ca919dd328b
AlucardFAS <FernandoImp@outlook.com.br>
2016-09-25 20:21:12 -0300

    Arquivo allegro criado, mas nada de jogo

---
+++ jogo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <allegro.h>
+
+int main()
+{
+  allegro_init();
+  install_timer();
+  install_keyboard();
+  set_color_depth(32);
+  set_gfx_model(GFX_AUTODETECT_WINDOWED, 800, 700, 0, 0);
+  set_window_title("Projeto Pyongyang");
+  
+  while(!key[KEY_ESC])
+  {
+    
+    
+    
+  }
+  return 0;
+}
+ END_OF_MAIN();
