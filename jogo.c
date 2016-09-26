3d784a2c5e9eccdde914ccc23ae551f514e20110
AlucardFAS <FernandoImp@outlook.com.br>
2016-09-26 20:18:16 -0300

    tamanho de tela corrigido 900x700

--- jogo.c
+++ jogo.c
@@ -7,7 +7,7 @@ int main()
   install_timer();
   install_keyboard();
   set_color_depth(32);
-  set_gfx_model(GFX_AUTODETECT_WINDOWED, 800, 700, 0, 0);
+  set_gfx_model(GFX_AUTODETECT_WINDOWED, 900, 700, 0, 0);
   set_window_title("Projeto Pyongyang");
   
   while(!key[KEY_ESC])
