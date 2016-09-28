87ca9b49e379a4305485dd1d0516c43b654eaafc
AlucardFAS <FernandoImp@outlook.com.br>
2016-09-28 20:41:38 -0300

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
-#include <allegro.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main()
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+ 
+int main(int argc, char *argv[])
 {
-  allegro_init();
-  install_timer();
-  install_keyboard();
-  set_color_depth(32);
-  set_gfx_model(GFX_AUTODETECT_WINDOWED, 900, 700, 0, 0);
-  set_window_title("Projeto Pyongyang");
-  
-  while(!key[KEY_ESC])
-  {
-    
-    
-    
-  }
-  return 0;
+    ALLEGRO_DISPLAY *janela = NULL;
+ 
+    if (!al_init())
+    {
+        fprintf(stderr, "Falha ao inicializar a Allegro 5\n");
+        return -1;
+    }
+ 
+    janela = al_create_display(900, 700);
+    if (!janela)
+    {
+        fprintf(stderr, "Falha ao criar a janela\n");
+        return -1;
+    }
+ 
+    al_clear_to_color(al_map_rgb(255, 255, 255));
+ 
+    al_flip_display();
+ 
+    al_rest(10.0);
+ 
+    al_destroy_display(janela);
+ 
+    return 0;
 }
- END_OF_MAIN();
