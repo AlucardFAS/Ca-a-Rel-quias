c04c5ebd80f25e1321693241259b73f8ea3e8002
AlucardFAS <FernandoImp@outlook.com.br>
2016-10-04 21:47:31 -0300

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -4,32 +4,34 @@
 
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_font.h>
  
 int main(int argc, char *argv[])
 {
-    ALLEGRO_DISPLAY *janela = NULL;
+	ALLEGRO_DISPLAY *janela = NULL;//janela principal
+	ALLEGRO_BITMAP *imagem = NULL;//imagem
  
-    if (!al_init())
-    {
-        fprintf(stderr, "Falha ao inicializar a Allegro 5\n");
-        return -1;
-    }
+	if (!al_init())
+	{
+		fprintf(stderr, "Falha ao inicializar a Allegro 5\n");
+		return -1;
+	}
  
-    janela = al_create_display(900, 700);
-    if (!janela)
+	al_init_image_addon();
+
+	janela = al_create_display(900, 700); //dimensão da janela
+	if (!janela)
     {
         fprintf(stderr, "Falha ao criar a janela\n");
         return -1;
     }
- 
-    al_clear_to_color(al_map_rgb(255, 255, 255));
- 
-    al_flip_display();
- 
+
+	
+	imagem = al_load_bitmap("background.jpg");//carregando imagem no programa
+
+	al_draw_bitmap(imagem,0,0,0);//carregando imagem para tela
+
+ 	al_flip_display();
     al_rest(10.0);
- 
     al_destroy_display(janela);
  
     return 0;
