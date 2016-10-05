337539cd218a0512e27a8e640bb0c7a437cf0dde
Victor Eleuterio Trindade <eleuteriotrindade@gmail.com>
2016-10-05 18:12:58 -0300

    Rodando com evente de fechar a janela

--- jogo.c
+++ jogo.c
@@ -1,73 +1,64 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-
-const int LARGURA_DE_TELA = 900;
-const int ALTURA_DE_TELA = 700;
-
-int main(int argc, char *argv[])
-{
-	ALLEGRO_DISPLAY *janela = NULL;//janela principal
-	ALLEGRO_BITMAP *imagem = NULL;//imagem
-	ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;//eventos
+ALLEGRO_DISPLAY *janela = NULL;
+    ALLEGRO_BITMAP *imagem = NULL;
+    ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
  
-	if (!al_init()) //CASO NÃO SEJA POSSÍVEL CARREGAR O ALLEGRO
-	{
-		fprintf(stderr, "Falha ao inicializar a Allegro 5\n");
-		return -1;
-	}
+    if (!al_init())
+    {
+        fprintf(stderr, "Falha ao inicializar a Allegro.\n");
+        return -1;
+    }
  
-	if (!al_init_image_addon()) //CASO NÃO SEJA POSSÍVEL CARREGAR IMAGENS
-	{
-		fprintf(stderr, "Falha ao inicializar add-on allegro_image.\n");
-		return -1;
-	}
-
-	janela = al_create_display(LARGURA_DE_TELA, ALTURA_DE_TELA); //dimensão da janela
-
-	if (!janela) //CASO A JANELA NÃO ABRA
-	{
-		fprintf(stderr, "Falha ao criar a janela\n");
-		return -1;
-	}
-	
-	imagem = al_load_bitmap("background.jpeg");//carregando imagem no programa
-	if (!imagem)//CASO A IMAGEM NÃO SEJA CARREGADA
-	{
-		fprintf(stderr, "Falha ao carregar o arquivo de imagem.\n");
-		al_destroy_display(janela);
-		return -1;
-	}
-
-
-	al_draw_bitmap(imagem,0,0,0);//carregando imagem para tela
- 	al_flip_display();
-	
-	while (1)
-	{
-		ALLEGRO_EVENT evento;
-		ALLEGRO_TIMEOUT timeout;
-		al_init_timeout(&timeout, 0.05);
-
-		int os_eventos = al_wait_for_event_until(fila_eventos,&evento,&timeout);
-		
-		if (os_eventos && evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-		{
-			break;
-		}
-		al_draw_bitmap(imagem,0,0,0);
-		al_flip_display();
-	}//chama a janela com a fila de eventos
-	al_destroy_event_queue(fila_eventos);
-	al_destroy_display(janela);
+    if (!al_init_image_addon())
+    {
+        fprintf(stderr, "Falha ao inicializar add-on allegro_image.\n");
+        return -1;
+    }
  
-	return 0;
-}
-
-
-
-
+    janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
+    if (!janela)
+    {
+        fprintf(stderr, "Falha ao criar janela.\n");
+        return -1;
+    }
+ 
+    imagem = al_load_bitmap("background.jpeg");
+    if (!imagem)
+    {
+        fprintf(stderr, "Falha ao carregar o arquivo de imagem.\n");
+        al_destroy_display(janela);
+        return -1;
+    }
+ 
+    fila_eventos = al_create_event_queue();
+    if (!fila_eventos)
+    {
+        fprintf(stderr, "Falha ao criar fila de eventos.\n");
+        al_destroy_display(janela);
+        return -1;
+    }
+ 
+    al_register_event_source(fila_eventos, al_get_display_event_source(janela));
+ 
+    al_draw_bitmap(imagem, 0, 0, 0);
+ 
+    al_flip_display();
+ 
+    while (1)
+    {
+        ALLEGRO_EVENT evento;
+        ALLEGRO_TIMEOUT timeout;
+        al_init_timeout(&timeout, 0.05);
+ 
+        int tem_eventos = al_wait_for_event_until(fila_eventos, &evento, &timeout);
+ 
+        if (tem_eventos && evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+        {
+            break;
+        }
+ 
+        al_draw_bitmap(imagem, 0, 0, 0);
+        al_flip_display();
+    }
+ 
+    al_destroy_display(janela);
+    al_destroy_event_queue(fila_eventos);
