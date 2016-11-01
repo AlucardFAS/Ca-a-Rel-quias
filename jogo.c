9d033d76ef987aa9ff057f78aebdd5010ef56f57
Victor Eleuterio Trindade <eleuteriotrindade@gmail.com>
2016-11-01 20:04:38 -0200

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -36,8 +36,9 @@ int jogo()
     int x=450,y=350;
     while (!sair)
     {
-        show_mouse(buffer);
+
         al_play_sample_instance(inst_trilha);//toca musica até o jogo ser fechado
+        
         while(!al_is_event_queue_empty(fila_eventos))
         {
             ALLEGRO_EVENT evento;
@@ -549,7 +550,7 @@ bool iniciar()
 
     al_attach_sample_instance_to_mixer(inst_trilha, al_get_default_mixer());//alocando o musica em um mixer padrão(Ubuntu)
     al_set_sample_instance_playmode(inst_trilha, ALLEGRO_PLAYMODE_LOOP);//loop para a musica.
-    al_set_sample_instance_gain(inst_trilha, 0.3);//volume da musica de fundo.
+    al_set_sample_instance_gain(inst_trilha, 0.5);//volume da musica de fundo.
 
  
     al_register_event_source(fila_eventos, al_get_keyboard_event_source());
