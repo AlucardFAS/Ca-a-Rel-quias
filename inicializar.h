87ac36265d4d6101337bf6b8a6ac567be9220097
Victor Eleuterio Trindade <eleuteriotrindade@gmail.com>
2016-11-26 10:02:26 -0200

    Update inicializar.h

--- inicializar.h
+++ inicializar.h
@@ -23,6 +23,7 @@ ALLEGRO_SAMPLE_INSTANCE *inst_trilha = NULL;
 ALLEGRO_BITMAP *livro = NULL;
 ALLEGRO_BITMAP *desc[5];
 ALLEGRO_BITMAP *hist[3];
+ALLEGRO_BITMAP *tela_inicio = NULL;
 
 bool iniciar()
 {
@@ -679,6 +680,41 @@ bool iniciar()
         al_destroy_bitmap(desc[4]);
         al_destroy_bitmap(hist[0]);
         al_destroy_bitmap(hist[1]);
+        return false;
+    }
+    tela_inicio= al_load_bitmap("itensfundos/telainicio.png");
+    if (!tela_inicio)
+    {
+        fprintf(stderr, "Falha ao carregar tela de inicio.\n");
+        al_destroy_display(janela);
+        al_destroy_event_queue(fila_eventos);
+        al_destroy_bitmap(fundo);
+        al_destroy_bitmap(esquerda[0]);
+        al_destroy_bitmap(direita[0]);
+        al_destroy_bitmap(esquerda[1]);   
+        al_destroy_bitmap(direita[1]);
+        al_destroy_bitmap(cima[0]);
+        al_destroy_bitmap(cima[1]);
+        al_destroy_bitmap(baixo[0]);
+        al_destroy_bitmap(baixo[1]);
+        al_destroy_sample(trilha);
+        al_destroy_sample_instance(inst_trilha);
+        al_destroy_bitmap(parado[0]);
+        al_destroy_bitmap(bau[0]);
+        al_destroy_bitmap(bau[1]);
+        al_destroy_bitmap(bau[2]);
+        al_destroy_bitmap(bau[3]);
+        al_destroy_bitmap(bau[4]);
+        al_destroy_bitmap(bau[5]);
+        al_destroy_bitmap(livro);
+        al_destroy_bitmap(desc[0]);
+        al_destroy_bitmap(desc[1]); 
+        al_destroy_bitmap(desc[2]);
+        al_destroy_bitmap(desc[3]);
+        al_destroy_bitmap(desc[4]);
+        al_destroy_bitmap(hist[0]);
+        al_destroy_bitmap(hist[1]);
+        al_destroy_bitmap(tela_inicio);
         return false;
     }
 
